71a49e2c373c94f2f856fc3531715c6690003fd0
Cuzz_0908 <b11223212@gemail.yuntech.edu.tw>
2025-04-09 23:55:14 +0800

    :sparkles: 新增自動執行.env的腳本指令

--- backend/src/main/java/iilabtime/backend/BackendApplication.java
+++ backend/src/main/java/iilabtime/backend/BackendApplication.java
@@ -9,15 +9,6 @@ public class BackendApplication {
 
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().directory("backend").filename(".env").ignoreIfMissing().load();
-        dotenv.entries().forEach(entry ->
-                System.setProperty(entry.getKey(), entry.getValue())
-        );
-        System.out.println(".env file load");
-        System.out.println("📦 以下是從 .env 載入的變數：");
-        dotenv.entries().forEach(entry -> {
-            System.out.println(entry.getKey() + "=" + entry.getValue());
-        });
         SpringApplication.run(BackendApplication.class, args);
     }
 
