2b8020d1820e3fec58c12c8a918c94dd69f62a7f
Cuzz_0908 <b11223212@gemail.yuntech.edu.tw>
2025-04-10 10:21:06 +0800

    :sparkles: 新增統一回傳格式API

--- backend/src/main/java/iilabtime/backend/Contorller/TestController.java
+++ backend/src/main/java/iilabtime/backend/Contorller/TestController.java
@@ -1,9 +1,8 @@
 package iilabtime.backend.Contorller;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import iilabtime.backend.ApiResponse;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/test")  // 基礎路徑為 /test
@@ -17,5 +16,10 @@ public class TestController {
     @GetMapping("/test2")
     public String test2() {
         return "Hello from test2";
+    }
+
+    @PostMapping("/test3")
+    public ApiResponse<String> test3(@RequestParam String ipt){
+        return ApiResponse.ok(ipt);
     }
 }
