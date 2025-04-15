09e8f5da93b7d7862834efb78e62da931bf356f0
xxgiasonxx <mnmilkim@gmail.com>
2025-04-15 21:24:50 +0800

    update navbar components

---
+++ hello-vue3/src/components/NavBar/MenuItem.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex items-center justify-center w-full h-full text-center p-10">
+        <router-link :to="router_path" class="font-bold" @click="HandleToggleMenu">{{ router_name
+            }}</router-link>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import { useStore } from 'vuex';
+
+defineProps({
+    router_path: {
+        type: String,
+        required: true
+    },
+    router_name: {
+        type: String,
+        required: true
+    }
+});
+
+const store = useStore(); // Access the Vuex store
+const HandleToggleMenu = () => {
+    store.dispatch("HandleMenuState", false); // Dispatch the action to toggle the menu state
+};
+</script>
