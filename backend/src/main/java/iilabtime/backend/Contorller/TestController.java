03f9fbc29724e1d119853c5bef03d3cd4fed5d41
Cuzz_0908 <b11223212@gemail.yuntech.edu.tw>
2025-04-16 20:05:08 +0800

    :sparkles: 新增抽象接口AuthApiController 讓他可以直接指定街口

--- backend/src/main/java/iilabtime/backend/Contorller/TestController.java
+++ backend/src/main/java/iilabtime/backend/Contorller/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@AuthApiController
 @RequestMapping("/test")  // 基礎路徑為 /test
 public class TestController {
 
