cb2180d0dc81086c52dd76d579d927a9b68247dd
Cuzz_0908 <b11223212@gemail.yuntech.edu.tw>
2025-04-16 19:56:53 +0800

    :sparkles: 新增抽象接口AuthBaseController 讓其他需要登入後的路由繼承她即可

---
+++ backend/src/main/java/iilabtime/backend/Contorller/AuthBaseController.java
@@ -0,0 +1,33 @@
+package iilabtime.backend.Contorller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/auth/api")
+public abstract class AuthBaseController {
+
+    private OAuth2User currentUser;
+
+    @ModelAttribute
+    public void setCurrentUser(@AuthenticationPrincipal OAuth2User user) {
+        this.currentUser = user;
+    }
+
+    protected OAuth2User getCurrentUser() {
+        return currentUser;
+    }
+
+    protected String getEmail() {
+        return currentUser != null ? currentUser.getAttribute("email") : null;
+    }
+
+    protected String getName() {
+        return currentUser != null ? currentUser.getAttribute("name") : null;
+    }
+
+    protected String getPicture() {
+        return currentUser != null ? currentUser.getAttribute("picture") : null;
+    }
+}
