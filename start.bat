c21358b58a1b820bf810c1ab05e535086955dde2
lindaline <68811165+lindaline@users.noreply.github.com>
2025-05-03 22:29:19 +0800

    fix google_login&setRealName

--- start.bat
+++ start.bat
@@ -4,6 +4,6 @@ set JAR_PATH=backend\target\backend-0.0.1-SNAPSHOT.jar
 
 echo Start（Profile=%PROFILE%）...
 docker compose up -d postgres-db
-java "-Dspring.profiles.active=%PROFILE%"-jar %JAR_PATH%
+java "-Dspring.profiles.active=%PROFILE%" -jar %JAR_PATH%
 
 pause
