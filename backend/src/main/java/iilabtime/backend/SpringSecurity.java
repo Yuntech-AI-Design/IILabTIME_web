84ae6dea70b9063661a54126221d307af16c23b2
Cuzz_0908 <b11223212@gemail.yuntech.edu.tw>
2025-05-15 20:06:48 +0800

    :sparkles: 新增登入成功之後跳轉指定路由

--- backend/src/main/java/iilabtime/backend/SpringSecurity.java
+++ backend/src/main/java/iilabtime/backend/SpringSecurity.java
@@ -2,6 +2,7 @@ package iilabtime.backend;
 
 import iilabtime.backend.Service.UserOAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,8 +16,12 @@ public class SpringSecurity {
     @Autowired
     private UserOAuthService userOAuthServicer;
 
+    @Value("${LOGIN_PATH}")
+    private String loginPath;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        System.out.println(loginPath);
         http
                 .csrf(AbstractHttpConfigurer::disable)
 
@@ -25,8 +30,11 @@ public class SpringSecurity {
                         .anyRequest().permitAll() // 其他都於須
               )
                 //.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2Login(oauth ->
-                        oauth.userInfoEndpoint(info -> info.userService(userOAuthServicer))
+                .oauth2Login(oauth ->{
+                            oauth.userInfoEndpoint(info -> info.userService(userOAuthServicer));
+                            oauth.defaultSuccessUrl(loginPath,true);
+                        }
+
                 )
                 .logout(logout -> logout
                         .logoutSuccessUrl("/")               // 登出後回首頁
